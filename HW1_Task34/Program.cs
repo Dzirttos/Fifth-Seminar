2e00ee85de6c4a78c054bf8d57c30547c8867e0d
Dizrtt <demonic2003@bk.ru>
2022-11-12 18:21:00 +0200

    Task 34 completed

--- HW1_Task34/Program.cs
+++ HW1_Task34/Program.cs
@@ -1,39 +1,19 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
-
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
-
 Console.Clear();
 
-int [] CreateRandomArray (int size, int start, int end)
+int[] CreateRandomArray(int size, int start, int end)
 {
-    int [] RandomArray = new int [size];
+    int[] RandomArray = new int[size];
     for (int i = 0; i < size; i++)
     {
-        RandomArray[i] = new Random().Next(start, end +1);
+        RandomArray[i] = new Random().Next(start, end + 1);
     }
     return RandomArray;
 }
 
-int [] CreateMyArray ()
-{
-    Console.WriteLine("Please enter array size:");
-    int size = Convert.ToInt32(Console.ReadLine());
-    int [] MyArray = new int [size];
-    for (int i = 0; i < size; i++)
-    {
-        Console.WriteLine($"Please enter array element № {i+1}:");
-        MyArray [i] = Convert.ToInt32(Console.ReadLine());
-    }
-    return MyArray;
-}
-
-void ShowArray(int [] array)
+void ShowArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -42,12 +22,30 @@ void ShowArray(int [] array)
     Console.WriteLine();
 }
 
-// int [] array = CreateRandomArray(9, -10, 10);
-// ShowArray(array);
-
-int [] array = CreateMyArray();
+System.Console.WriteLine("Please enter array size:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(size, 100, 999);
 ShowArray(array);
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+if (count == 1)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"There is only {count} even number in array");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"There are {count} even numbers in array");
+}
 
 
 
-    
+
